289dddf82597b907929f94d52b6883b4cd5587a5
AllisonL1u <33563100+AllisonL1u@users.noreply.github.com>
2018-12-19 17:43:24 +0800

    Update main.js

--- work/2019test/js/main.js
+++ work/2019test/js/main.js
@@ -1,4 +1,7 @@
-function formatNum(money) {
+/**
+ * Created by Allison on 2018/12/19.
+ */
+ function formatNum(money) {
     for (var a = ""; money > 999;) {
         var s = money.toString();
         a = "," + s.substring(s.length - 3) + a;
@@ -7,6 +10,7 @@ function formatNum(money) {
 
     return money + a;
 }
+
 function showCurrentTime() {
     var time = new Date;
     var time = time.toLocaleDateString() + time.toLocaleTimeString();
@@ -14,105 +18,54 @@ function showCurrentTime() {
 }
 
 function loadData() {
-    $.ajax({
-        url: "https://www.google.com",
+	$.ajax({
+		url: "http://www.google.com",
         type: "GET",
         dataType: 'JSON',
         success: function (result) {
-            result["gmv_redeem_xidian"] = parseInt(result["gmv_redeem_amount"] * 0.35);
-            result["gmv_total_amount"] = parseInt(
-                result["gmv_album_amount"] +
-                result["gmv_vip_amount"] +
-                result["gmv_recharge_remain_amount"] +
-                result["gmv_redeem_amount"]
-            );
-            result["gmv_total_xidian"] = parseInt(
-                result["gmv_album_xidian"] +
-                result["gmv_vip_amount"] +
-                result["gmv_recharge_remain_amount"] +
-                result["gmv_redeem_xidian"]
-            );
-
-
-            result["gmv_redeem_xidian_yesterday"] = parseInt(result["gmv_redeem_yesterday"] * 0.35);
-            result["gmv_total_yesterday"] = parseInt(
-                result["gmv_album_yesterday"] +
-                result["gmv_vip_yesterday"] +
-                result["gmv_recharge_remain_yesterday"] +
-                result["gmv_redeem_yesterday"]
-            );
-            result["gmv_total_xidian_yesterday"] = parseInt(
-                result["gmv_album_xidian_yesterday"] +
-                result["gmv_vip_yesterday"] +
-                result["gmv_recharge_remain_yesterday"] +
-                result["gmv_redeem_xidian_yesterday"]
-            );
-
-
-            result["gmv_album_rate"] = parseInt((result["gmv_album_amount"] / result["gmv_album_yesterday"] - 1) * 100);
-            result["gmv_album_xidian_rate"] = parseInt((result["gmv_album_xidian"] / result["gmv_album_xidian_yesterday"] - 1) * 100);
-            result["gmv_redeem_rate"] = parseInt((result["gmv_redeem_amount"] / result["gmv_redeem_yesterday"] - 1) * 100);
-            result["gmv_redeem_xidian_rate"] = parseInt((result["gmv_redeem_xidian"] / result["gmv_redeem_xidian_yesterday"] - 1) * 100);
-            result["gmv_vip_rate"] = parseInt((result["gmv_vip_amount"] / result["gmv_vip_yesterday"] - 1) * 100);
-            result["gmv_recharge_remain_rate"] = parseInt((result["gmv_recharge_remain_amount"] / result["gmv_recharge_remain_yesterday"] - 1) * 100);
-            result["gmv_total_rate"] = parseInt((result["gmv_total_amount"] / result["gmv_total_yesterday"] - 1) * 100);
-            result["gmv_total_xidian_rate"] = parseInt((result["gmv_total_xidian"] / result["gmv_total_xidian_yesterday"] - 1) * 100);
-            result["gmv_recharge_rate"] = parseInt((result["gmv_recharge"] / result["gmv_recharge_yesterday"] - 1) * 100);
-
-
-            result["yesterday_gmv_redeem_xidian"] = parseInt(result["yesterday_gmv_redeem_amount"] * 0.35);
-            result["yesterday_gmv_total"] = parseInt(
-                result["yesterday_gmv_album_amount"] +
-                result["yesterday_gmv_vip_amount"] +
-                result["yesterday_gmv_recharge_remain_amount"] +
-                result["yesterday_gmv_redeem_amount"]
-            );
-            result["yesterday_gmv_total_xidian"] = parseInt(
-                result["yesterday_gmv_album_xidian"] +
-                result["yesterday_gmv_vip_amount"] +
-                result["yesterday_gmv_recharge_remain_amount"] +
-                result["yesterday_gmv_redeem_xidian"]
-            );
-            var rateKeys = ["gmv_album_rate", "gmv_album_xidian_rate", "gmv_redeem_rate", "gmv_redeem_xidian_rate",
-                "gmv_vip_rate", "gmv_recharge_remain_rate", "gmv_total_rate", "gmv_total_xidian_rate", "gmv_recharge_rate"];
-
-            for (var key in result) {
-                if ($.inArray(key, rateKeys) > "-1") {
-                    if (result[key] > 0) {
-                        var span = $("<span></span>");
-                        var rate = $("." + key);
-                        rate.empty();
-                        rate.text(" +" + result[key] + "%");
-                        rate.css({
-                            "color": "red",
-                            "font-size":"small"
-                        });
-                        rate.prepend(span);
-                    }
-                    else {
-                        var span = $("<span></span>");
-                        var rate = $("." + key);
-                        rate.empty();
-                        rate.text(" " + result[key] + "%");
-                        rate.css({
-                            "color": "green",
-                            "font-size":"small"
-                        });
-                        rate.prepend(span);
-                    }
-
-                }
-                else {
-                    $("." + key).text(formatNum(result[key]));
-                    $("." + key).css("font-size","medium")
-                }
-            }
-
-
+        	result['nano_1'] = parseInt(result['aaa'])
+        	result['nano_2'] = parseInt(result['aaa'])
+        	result['nano_3'] = parseInt(result['aaa'])
+        	result['15_days_vip_free'] = parseInt(result['aaa'])
+        	result['vip_purchase'] = parseInt(result['aaa'])
+        	result['vip_gmv'] = parseInt(result['aaa'])
+        	result['album_purchase_gmv'] = parseInt(result['aaa'])
+        	result['kol_course_pv'] = parseInt(result['aaa'])
+        	result['kol_course_uv'] = parseInt(result['aaa'])
+        	result['lunch_pv'] = parseInt(result['aaa'])
+        	result['lunch_uv'] = parseInt(result['aaa'])
+        	for (var key in result) {
+        		$("." + key).text(formatNum(result[key]));
+                $("." + key).css("font-size","small");
+        	}
         }
-    });
+	});
+	$(.nano_1).text(formatNum(999999999999));
+    $(.nano_1).css("font-size","small");
+    $(.nano_2).text(formatNum(999999999999));
+    $(.nano_2).css("font-size","small");
+    $(.nano_3).text(formatNum(999999999999));
+    $(.nano_3).css("font-size","small");
+    $(.15_days_vip_free).text(formatNum(999999999999));
+    $(.15_days_vip_free).css("font-size","small");
+    $(.vip_purchase).text(formatNum(999999999999));
+    $(.vip_purchase).css("font-size","small");
+    $(.vip_gmv).text(formatNum(999999999999));
+    $(.vip_gmv).css("font-size","small");
+    $(.album_purchase_gmv).text(formatNum(999999999999));
+    $(.album_purchase_gmv).css("font-size","small");
+    $(.kol_course_pv).text(formatNum(999999999999));
+    $(.kol_course_pv).css("font-size","small");
+    $(.kol_course_uv).text(formatNum(999999999999));
+    $(.kol_course_uv).css("font-size","small");
+    $(.lunch_pv).text(formatNum(999999999999));
+    $(.lunch_pv).css("font-size","small");
+    $(.lunch_uv).text(formatNum(999999999999));
+    $(.lunch_uv).css("font-size","small");
 }
+
 showCurrentTime();
 setInterval(showCurrentTime, 1000);
 loadData();
 setInterval(loadData, 5000);
+
